7176fa7eedda5e4233fa7bdd58e7bf53af0b88b9
David Connolly <david@connol.ly>
2026-09-20 22:05:27 +0100

    mb/asrock/z77_extreme4: embed a sample OC profile
    DDR3-1600, 1.65 V, 9-9-9-24, tRFC 160, 1N.  Replace the file with
    cbfstool or regenerate it with tools/rebuild/coreboot/mkprofile.py.

--- src/mainboard/asrock/z77_extreme4/Makefile.mk
+++ src/mainboard/asrock/z77_extreme4/Makefile.mk
@@ -1,5 +1,9 @@
 ## SPDX-License-Identifier: GPL-2.0-only
 
+cbfs-files-$(CONFIG_NATIVE_RAMINIT_OC_PROFILE) += oc_memory_profile.bin
+oc_memory_profile.bin-file := $(src)/mainboard/$(MAINBOARDDIR)/oc_memory_profile.bin
+oc_memory_profile.bin-type := raw
+
 bootblock-y += gpio.c
 romstage-y += gpio.c
 
